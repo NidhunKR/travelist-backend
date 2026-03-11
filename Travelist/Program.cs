108d2cfec9220b44392dbf2246e84f465e12bd03
Nidhun K R <nidhun.karaparambil.ramakrishnan@gmail.com>
2026-03-12 01:40:16 +0530

    Allow frontend production URL

--- Travelist/Program.cs
+++ Travelist/Program.cs
@@ -18,7 +18,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000",
+                "https://travelist-frontend.onrender.com"
+                )
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
